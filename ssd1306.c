eaa48b2b3f987ec070d241f6f24b95e41f7d2d50
fiting <fitingnot@gmail.com>
2026-07-03 13:52:31 +0300

    fix2

--- ssd1306.c
+++ ssd1306.c
@@ -7,8 +7,6 @@
 static i2c_inst_t *current_i2c;
 static uint8_t current_addr;
 
-static uint8_t frame_buffer[1024];
-
 static void send_init_commands(void) {
     uint8_t init_cmds[] = {
         0x00, 0xAE, 0xD5, 0x80, 0xA8, 0x3F, 0xD3, 0x00, 
@@ -19,12 +17,12 @@ static void send_init_commands(void) {
     i2c_write_blocking(current_i2c, current_addr, init_cmds, sizeof(init_cmds), false);
 }
 
-void oled_init(ssd1306_t *p, i2c_inst_t *i2c, uint8_t addr, uint_fast8_t sda_pin, uint_fast8_t scl_pin, uint8_t widh, uint8_t heigh) {
+void oled_init(ssd1306_t *p, i2c_inst_t *i2c, uint8_t addr, uint_fast8_t sda_pin, uint_fast8_t scl_pin, uint8_t width, uint8_t height ) {
     current_i2c = i2c;
     current_addr = addr;
 
-    p->width = widh;
-    p->height = heigh;
+    p->width = width;
+    p->height = height;
     uint32_t buffer_size = (p->width * p->height) / 8;
     p->buffer = malloc(buffer_size);
 
@@ -43,19 +41,19 @@ void ssd1306_draw_pixel(ssd1306_t *p, uint32_t x, uint32_t y) {
     p->buffer[x + p->width * (y >> 3)] |= 0x1 << (y & 0x07); 
 }
 void ssd1306_show(ssd1306_t *p) {
-    uint8_t temp_buf[1025];
-    
+    uint32_t buffer_size = (p->width * p->height) / 8;
+    uint8_t temp_buf[1025]; // 1 byte for control byte + 1024 bytes for display buffer
     temp_buf[0] = 0x40;
     
-    for (int i = 0; i < 1024; i++) {
+    for (int i = 0; i < buffer_size; i++){
         temp_buf[i + 1] = p->buffer[i];
-
     }
 
     i2c_write_blocking(current_i2c, current_addr, temp_buf, sizeof(temp_buf), false);
 }
 void ssd1306_clear(ssd1306_t *p) {
-    for (int i = 0; i < 1024; i++) {
+    uint32_t buffer_size = (p->width * p->height) / 8;
+    for (int i = 0; i < buffer_size; i++) {
         p->buffer[i] = 0;
     }
 }
