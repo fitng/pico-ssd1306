c1f1e94bb1f5ea5f94743eee6889f8e435a292d9
fiting <fitingnot@gmail.com>
2026-07-03 13:54:28 +0300

    add width and height

--- ssd1306.h
+++ ssd1306.h
@@ -10,7 +10,7 @@ typedef struct {
     uint8_t *buffer;
 } ssd1306_t;
 
-void oled_init(ssd1306_t *p, i2c_inst_t *i2c, uint8_t addr, uint_fast8_t sda_pin, uint_fast8_t scl_pin, uint8_t widh, uint8_t heigh);
+void oled_init(ssd1306_t *p, i2c_inst_t *i2c, uint8_t addr, uint_fast8_t sda_pin, uint_fast8_t scl_pin, uint8_t width, uint8_t height);
 
 void ssd1306_draw_pixel(ssd1306_t *p, uint32_t x, uint32_t y);
 
